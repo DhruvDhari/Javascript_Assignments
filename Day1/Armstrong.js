fb6079b46c1b2f5af93253a1848a5092d8bd6c33
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-07-24 18:19:20 +0530

    Armstrong Number Compute Function

---
+++ Day1/Armstrong.js
@@ -0,0 +1,23 @@
+let number;
+number = "153"; 
+let n = number.length;
+
+
+let sum = 0; 
+
+let i=0;
+
+while(i<n)
+{
+    let digit = number[i]; 
+    sum += Math.pow(digit,n);
+    i++; 
+}
+
+if(sum == number)
+{
+    console.log(number, "is an Armstrong Number");
+}
+else{
+    console.log(number, "is not an Armstrong Number");
+}
