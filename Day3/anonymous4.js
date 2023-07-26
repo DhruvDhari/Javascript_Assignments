d481c967b0cea61587886d5e51d6eb1e581e1f6c
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-07-27 01:16:21 +0530

    all questions done for today !!!

---
+++ Day3/anonymous4.js
@@ -0,0 +1,11 @@
+let x = function () {  
+    let today=new Date();
+    let day=today.getDate();
+    let month=today.getMonth()+1;
+    let year=today.getFullYear();
+
+    console.log(`${day}/${month}/${year}`);
+
+};  
+
+x();
