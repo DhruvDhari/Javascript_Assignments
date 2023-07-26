6992655541ea7918c58326868978267b26b486c5
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-07-27 01:07:37 +0530

    added anonymous3.js file

---
+++ Day3/anonymous3.js
@@ -0,0 +1,14 @@
+let x = function () {  
+    let x="AM";
+    let mydays ={0:"Sunday", 1: "Monday", 2: "Tuesday", 3: "Wednesday",4:"Thursday",5:"Friday",6:"Saturday"};
+    let date = new Date();
+    let day = date.getDay();
+    if(date.getHours()>=12){
+        x="PM";
+    }
+    
+    console.log("Today is",mydays[day])
+    console.log(`Current time is : ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${x}`);
+};  
+
+x();
