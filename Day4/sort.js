d0662422d529114510bacd5b1d870259f3c837af
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-07-27 19:39:22 +0530

    added and updated sort.js

---
+++ Day4/sort.js
@@ -0,0 +1,10 @@
+let emp=[
+    {name:"aman",salary:"10000"},
+    {name:"akshat",salary:"3000"},
+    {name:"tushar",salary:"2000"},
+    {name:"akhil",salary:"12000"}
+]
+
+emp.sort((a,b)=>a.salary-b.salary);
+console.log(emp);
+
