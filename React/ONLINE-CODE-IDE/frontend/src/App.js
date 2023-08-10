3672dd45a723a61be39c70bf04356213c0c964d8
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-10 10:25:10 +0530

    added new project

---
+++ React/ONLINE-CODE-IDE/frontend/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Container from '@mui/material/Container';
+
+function App() {
+  return (
+    <>
+    <Container ></Container>
+    </>
+  );
+}
+
+export default App;
