0811484d0f8d5084bc5c019194f7a8882bd66401
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-14 11:07:54 +0530

    updated

--- React/ONLINE-CODE-IDE/node-code/one.js
+++ React/ONLINE-CODE-IDE/node-code/one.js
@@ -2,7 +2,7 @@
 
 console.log("start")
 const fs =require('fs');
-const filepath='C:\Users\lenono\OneDrive\Desktop\COLLEGE_MERN\My_Assignments\Javascript_Assignments\React\ONLINE-CODE-IDE\node-code\sample.txt'
+const filepath='Users\lenono\OneDrive\Desktop\COLLEGE_MERN\My_Assignments\Javascript_Assignments\React\ONLINE-CODE-IDE\node-code\sample.txt'
 fs.readFile(filepath,(err,content)=>{
     if(err){
         console.log('unable to read file ',err);
@@ -10,4 +10,5 @@ fs.readFile(filepath,(err,content)=>{
         console.log('file content is ',content.toString);
     }
 }); //Async hoga
+console.log("current :",filepath);
 console.log("end");
