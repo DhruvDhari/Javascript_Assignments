2a1ae0655e603923933a3eaf6a36d61122a138b2
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-16 11:25:37 +0530

    updated

--- React/ONLINE-CODE-IDE/backend/app.js
+++ React/ONLINE-CODE-IDE/backend/app.js
@@ -1,25 +1,34 @@
-const express =require('express');
-// import express from 'express';
+// const express =require('express');
+import express from 'express';
+import { userRoutes } from './routes/user-routes.js';
+import {ideRoutes} from './routes/ide-routes.js';
 
 const app=express();
 
-app.use(express.static("public"));
-
-app.get('/home',(request,response)=>{
-    response.send("hello this is a server")
+// app.use(express.static("public"));
+app.use('/',userRoutes)
+app.use('/',ideRoutes)
+app.use((request,response,next)=>{
+    response.json({message :'invalid url'})
 })
 
-app.get('/urljson',(request,response)=>{
-    response.json({'question':'this is a question','answer':'something'})
-    // response.json({'answer':'mujhe kya pta '})
-})
+// app.get('/home',(request,response)=>{
+//     response.send("hello this is a server")
+// })
 
-app.listen(4000,(err)=>{
+// app.get('/urljson',(request,response)=>{
+//     response.json({'question':'this is a question','answer':'something'})
+//     // response.json({'answer':'mujhe kya pta '})
+// })
+
+
+
+const server=app.listen(1234,(err)=>{
     if(err){
-        console.log("harkat spotted");
+        console.log("Server Crash ",err);
     }
     else{
-        console.log("server started at port number 4000");
+        console.log("server Up and Running on",server.address().port);
     }
     
 })
---
+++ React/ONLINE-CODE-IDE/backend/routes/ide-routes.js
@@ -0,0 +1,5 @@
+import express from 'express';
+
+export const ideRoutes = express.Router();
+
+ideRoutes.post('/compile')
