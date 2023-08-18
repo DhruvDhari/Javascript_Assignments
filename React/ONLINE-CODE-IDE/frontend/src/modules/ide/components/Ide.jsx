eba9340330dffee0675ebb7afdc28f4c5772d6f2
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-19 00:52:48 +0530

    update

--- React/ONLINE-CODE-IDE/frontend/src/modules/ide/components/Ide.jsx
+++ React/ONLINE-CODE-IDE/frontend/src/modules/ide/components/Ide.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import Editor from '@monaco-editor/react';
+import Button from '@mui/material/Button';
+import { useRef } from 'react';
 
 export const Ide = () => {
+
+    const editorRef =useRef(null);
+
+    const mount=(editor)=>{
+        editorRef.current=editor;
+    }
+
+    const getCode=()=>{
+        const code =editorRef.current.getValue();
+        console.log('Code is ',code);
+    }
+
 
     const skeletonCode=`
     class Solution{
@@ -12,11 +26,13 @@ export const Ide = () => {
   return (
     <>
     <Editor
-        height="70vh"
-        defaultLanguage="java"
-        defaultValue={skeletonCode}
+     onMount ={mount}
+     height="70vh"
+     defaultLanguage="java"
+     defaultValue={skeletonCode}
         
       />
+     <Button onClick={getCode} variant="contained">Submit</Button> 
     </>
   )
 }
