6dd2bf15b81077251db4c427e8d6558f7bc7db33
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-19 11:36:27 +0530

    update

--- React/ONLINE-CODE-IDE/node-code/one.js
+++ React/ONLINE-CODE-IDE/node-code/one.js
@@ -2,7 +2,7 @@ console.log('hi node js');
 
 // console.log("start")
 // const fs =require('fs');
-// const filepath='Users\lenono\OneDrive\Desktop\COLLEGE_MERN\My_Assignments\Javascript_Assignments\React\ONLINE-CODE-IDE\node-code\sample.txt'
+// const filepath='Users/lenono/OneDrive/Desktop/COLLEGE_MERN/My_Assignments/Javascript_Assignments/React/ONLINE-CODE-IDE/node-code/sample.txt'
 // fs.readFile(filepath,(err,content)=>{
 //     if(err){
 //         console.log('unable to read file ',err);
@@ -12,3 +12,16 @@ console.log('hi node js');
 // }); //Async hoga
 // console.log("current :",filepath);
 // console.log("end");
+console.log("yeh toh buss shuruwat h")
+const fs=require('fs');
+const filepath='C:/Users/lenono/OneDrive/Desktop/COLLEGE_MERN/My_Assignments/Javascript_Assignments/React/ONLINE-CODE-IDE/node-code/sample.txt'
+fs.readFile(filepath,(err,content)=>{
+    if(err){
+        console.log('Unable to read file',err);
+    }
+    else{
+        console.log('File Content is ',content.toString());
+    }
+});//async style mai hoga
+
+console.log('code ends');
