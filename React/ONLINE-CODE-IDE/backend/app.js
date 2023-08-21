4ed787c0df81cd47c699d5f1dde1413e41a031bd
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-21 10:46:47 +0530

    updated

--- React/ONLINE-CODE-IDE/backend/app.js
+++ React/ONLINE-CODE-IDE/backend/app.js
@@ -1,34 +1,35 @@
-// const express =require('express');
-import express from 'express';
-import { userRoutes } from './routes/user-routes.js';
-import {ideRoutes} from './routes/ide-routes.js';
+// // const express =require('express');
+// import express from 'express';
+// import { userRoutes } from './routes/user-routes.js';
+// import {ideRoutes} from './routes/ide-routes.js';
 
-const app=express();
+// const app=express();
 
-// app.use(express.static("public"));
-app.use('/',userRoutes)
-app.use('/',ideRoutes)
-app.use((request,response,next)=>{
-    response.json({message :'invalid url'})
-})
-
-// app.get('/home',(request,response)=>{
-//     response.send("hello this is a server")
+// // app.use(express.static("public"));
+// app.use('/',userRoutes)
+// app.use('/',ideRoutes)
+// app.use((request,response,next)=>{
+//     response.json({message :'invalid url'})
 // })
 
-// app.get('/urljson',(request,response)=>{
-//     response.json({'question':'this is a question','answer':'something'})
-//     // response.json({'answer':'mujhe kya pta '})
-// })
+// // app.get('/home',(request,response)=>{
+// //     response.send("hello this is a server")
+// // })
+
+// // app.get('/urljson',(request,response)=>{
+// //     response.json({'question':'this is a question','answer':'something'})
+// //     // response.json({'answer':'mujhe kya pta '})
+// // })
 
 
 
-const server=app.listen(1234,(err)=>{
-    if(err){
-        console.log("Server Crash ",err);
-    }
-    else{
-        console.log("server Up and Running on",server.address().port);
-    }
+// const server=app.listen(1234,(err)=>{
+//     if(err){
+//         console.log("Server Crash ",err);
+//     }
+//     else{
+//         console.log("server Up and Running on",server.address().port);
+//     }
     
-})
+// })
+
--- React/ONLINE-CODE-IDE/node-code/two.js
+++ React/ONLINE-CODE-IDE/node-code/two.js
@@ -1,2 +1,14 @@
 const fs =require('fs');
-fs.readFile();
+// fs.readFile();
+
+// to do streaming
+// const stream=fs.createReadStream(path,{higherWaterMark:300});
+//stream.on('open',()=>{
+// console.log('stream open ');
+// })
+//stream.on('data',(chunk)=>{
+// console.log('chunk is :',chunk);
+// })
+//stream.on('end',()=>{
+// console.log('stream ends');
+// })
