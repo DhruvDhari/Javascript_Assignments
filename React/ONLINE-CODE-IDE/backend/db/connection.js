9021f8e0a8270131323788382e1b2192801f95c0
Dhruv Dhari <dhruvdhari2003@gmail.com>
2023-08-22 20:00:47 +0530

    updated

--- React/ONLINE-CODE-IDE/backend/db/connection.js
+++ React/ONLINE-CODE-IDE/backend/db/connection.js
@@ -1,1 +1,10 @@
 const url='mongodb+srv://admin:admin123@mycluster.edilkzb.mongodb.net/userdb?retryWrites=true&w=majority';
+
+import mongoose from "mongoose";
+const promise = mongoose.connect(url);
+promise.then(data=>{
+    console.log('DB Connected ...')
+}).catch(err=>{
+    console.log('Error in Connecting to DB ',err);
+}
+)
